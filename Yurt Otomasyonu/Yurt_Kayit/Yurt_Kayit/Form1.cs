859292d5560a0a657d6c0398950775e0b0af99e6
Ayşegül <115697392+aysegulozden@users.noreply.github.com>
2023-04-11 17:29:25 +0300

    Add files via upload

---
+++ Yurt Otomasyonu/Yurt_Kayit/Yurt_Kayit/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Yurt_Kayit
+{
+    public partial class FrmOgrKayit : Form
+    {
+        public FrmOgrKayit()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-SJJ7N21\\SQLEXPRESS;Initial Catalog=YürtKayit;Integrated Security=True");
+
+        private void FrmOgrKayit_Load(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("Select BolumAd From Bolumler",baglanti);
+            SqlDataReader oku = komut.ExecuteReader();
+            while(oku.Read())
+            {
+                CmbBolum.Items.Add(oku[0].ToString());
+            }
+            baglanti.Close();
+
+        }
+    }
+}
+//Data Source=DESKTOP-SJJ7N21\SQLEXPRESS;Initial Catalog=YürtKayit;Integrated Security=True
