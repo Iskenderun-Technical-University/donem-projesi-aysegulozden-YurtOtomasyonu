9a9e99ea031f6ed451f026119d9338cf75fdedae
Ayşegül <115697392+aysegulozden@users.noreply.github.com>
2023-04-14 15:26:36 +0300

    Add files via upload

--- Yurt Otomasyonu/Yurt_Kayit/Yurt_Kayit/Form1.Designer.cs
+++ Yurt Otomasyonu/Yurt_Kayit/Yurt_Kayit/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.GrbOgrBilgi = new System.Windows.Forms.GroupBox();
-            this.CmbOda = new System.Windows.Forms.ComboBox();
+            this.CmbOdaNo = new System.Windows.Forms.ComboBox();
             this.MskTelefonOgr = new System.Windows.Forms.MaskedTextBox();
             this.label9 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
@@ -47,15 +47,15 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label10 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label11 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
             this.MskVeliTelefon = new System.Windows.Forms.MaskedTextBox();
             this.label12 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.TxtVeliAd = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
             this.BtnKaydet = new System.Windows.Forms.Button();
+            this.TxtVeliSoyad = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.GrbOgrBilgi.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -63,7 +63,7 @@
             // GrbOgrBilgi
             // 
             this.GrbOgrBilgi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.GrbOgrBilgi.Controls.Add(this.CmbOda);
+            this.GrbOgrBilgi.Controls.Add(this.CmbOdaNo);
             this.GrbOgrBilgi.Controls.Add(this.MskTelefonOgr);
             this.GrbOgrBilgi.Controls.Add(this.label9);
             this.GrbOgrBilgi.Controls.Add(this.label8);
@@ -87,29 +87,28 @@
             this.GrbOgrBilgi.TabStop = false;
             this.GrbOgrBilgi.Text = "Öğrenci Bilgileri";
             // 
-            // CmbOda
+            // CmbOdaNo
             // 
-            this.CmbOda.FormattingEnabled = true;
-            this.CmbOda.Location = new System.Drawing.Point(155, 342);
-            this.CmbOda.Name = "CmbOda";
-            this.CmbOda.Size = new System.Drawing.Size(279, 24);
-            this.CmbOda.TabIndex = 34;
+            this.CmbOdaNo.FormattingEnabled = true;
+            this.CmbOdaNo.Location = new System.Drawing.Point(155, 342);
+            this.CmbOdaNo.Name = "CmbOdaNo";
+            this.CmbOdaNo.Size = new System.Drawing.Size(279, 24);
+            this.CmbOdaNo.TabIndex = 34;
             // 
             // MskTelefonOgr
             // 
             this.MskTelefonOgr.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.MskTelefonOgr.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.MskTelefonOgr.Location = new System.Drawing.Point(155, 207);
-            this.MskTelefonOgr.Mask = "00/00/0000";
+            this.MskTelefonOgr.Location = new System.Drawing.Point(155, 156);
+            this.MskTelefonOgr.Mask = "(999) 000-0000";
             this.MskTelefonOgr.Name = "MskTelefonOgr";
             this.MskTelefonOgr.Size = new System.Drawing.Size(279, 26);
             this.MskTelefonOgr.TabIndex = 33;
-            this.MskTelefonOgr.ValidatingType = typeof(System.DateTime);
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(32, 212);
+            this.label9.Location = new System.Drawing.Point(19, 161);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(108, 16);
             this.label9.TabIndex = 32;
@@ -163,7 +162,7 @@
             // 
             this.MskDogumTarihi.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.MskDogumTarihi.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.MskDogumTarihi.Location = new System.Drawing.Point(155, 162);
+            this.MskDogumTarihi.Location = new System.Drawing.Point(155, 203);
             this.MskDogumTarihi.Mask = "00/00/0000";
             this.MskDogumTarihi.Name = "MskDogumTarihi";
             this.MskDogumTarihi.Size = new System.Drawing.Size(279, 26);
@@ -173,7 +172,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(32, 167);
+            this.label5.Location = new System.Drawing.Point(32, 208);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(95, 16);
             this.label5.TabIndex = 25;
@@ -245,12 +244,12 @@
             // 
             this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.TxtVeliSoyad);
+            this.groupBox1.Controls.Add(this.label10);
             this.groupBox1.Controls.Add(this.label13);
             this.groupBox1.Controls.Add(this.MskVeliTelefon);
             this.groupBox1.Controls.Add(this.label12);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.label10);
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.TxtVeliAd);
             this.groupBox1.Controls.Add(this.label11);
             this.groupBox1.Location = new System.Drawing.Point(12, 396);
             this.groupBox1.Name = "groupBox1";
@@ -259,76 +258,50 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Veli Bilgileri";
             // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textBox1.Location = new System.Drawing.Point(155, 66);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(279, 26);
-            this.textBox1.TabIndex = 26;
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(46, 76);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(81, 16);
-            this.label10.TabIndex = 25;
-            this.label10.Text = "Veli Soyadı:";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textBox2.Location = new System.Drawing.Point(155, 21);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(279, 26);
-            this.textBox2.TabIndex = 24;
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(66, 31);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(61, 16);
-            this.label11.TabIndex = 23;
-            this.label11.Text = "Veli Adı:";
-            // 
-            // MskVeliTelefon
-            // 
-            this.MskVeliTelefon.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.MskVeliTelefon.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.MskVeliTelefon.Location = new System.Drawing.Point(155, 112);
-            this.MskVeliTelefon.Mask = "00/00/0000";
-            this.MskVeliTelefon.Name = "MskVeliTelefon";
-            this.MskVeliTelefon.Size = new System.Drawing.Size(279, 26);
-            this.MskVeliTelefon.TabIndex = 35;
-            this.MskVeliTelefon.ValidatingType = typeof(System.DateTime);
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(42, 117);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(85, 16);
-            this.label12.TabIndex = 34;
-            this.label12.Text = "Veli Telefon:";
-            // 
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(42, 157);
+            this.label13.Location = new System.Drawing.Point(42, 150);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(76, 16);
             this.label13.TabIndex = 36;
             this.label13.Text = "Veli Adres:";
             // 
-            // richTextBox1
+            // MskVeliTelefon
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(155, 144);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(279, 96);
-            this.richTextBox1.TabIndex = 37;
-            this.richTextBox1.Text = "";
+            this.MskVeliTelefon.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MskVeliTelefon.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.MskVeliTelefon.Location = new System.Drawing.Point(155, 108);
+            this.MskVeliTelefon.Mask = "(999) 000-0000";
+            this.MskVeliTelefon.Name = "MskVeliTelefon";
+            this.MskVeliTelefon.Size = new System.Drawing.Size(279, 26);
+            this.MskVeliTelefon.TabIndex = 35;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(33, 108);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(85, 16);
+            this.label12.TabIndex = 34;
+            this.label12.Text = "Veli Telefon:";
+            // 
+            // TxtVeliAd
+            // 
+            this.TxtVeliAd.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtVeliAd.Location = new System.Drawing.Point(155, 27);
+            this.TxtVeliAd.Name = "TxtVeliAd";
+            this.TxtVeliAd.Size = new System.Drawing.Size(279, 26);
+            this.TxtVeliAd.TabIndex = 24;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(57, 32);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(61, 16);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "Veli Adı:";
             // 
             // BtnKaydet
             // 
@@ -340,6 +313,32 @@
             this.BtnKaydet.TabIndex = 20;
             this.BtnKaydet.Text = "Kaydet";
             this.BtnKaydet.UseVisualStyleBackColor = false;
+            this.BtnKaydet.Click += new System.EventHandler(this.BtnKaydet_Click);
+            // 
+            // TxtVeliSoyad
+            // 
+            this.TxtVeliSoyad.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtVeliSoyad.Location = new System.Drawing.Point(155, 64);
+            this.TxtVeliSoyad.Name = "TxtVeliSoyad";
+            this.TxtVeliSoyad.Size = new System.Drawing.Size(279, 26);
+            this.TxtVeliSoyad.TabIndex = 39;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(37, 69);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(81, 16);
+            this.label10.TabIndex = 38;
+            this.label10.Text = "Veli Soyadı:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(155, 150);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(279, 96);
+            this.richTextBox1.TabIndex = 40;
+            this.richTextBox1.Text = "";
             // 
             // FrmOgrKayit
             // 
@@ -351,7 +350,7 @@
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.GrbOgrBilgi);
             this.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmOgrKayit";
             this.Text = "Öğrenci Kayıt";
             this.Load += new System.EventHandler(this.FrmOgrKayit_Load);
@@ -366,7 +365,7 @@
         #endregion
 
         private System.Windows.Forms.GroupBox GrbOgrBilgi;
-        private System.Windows.Forms.ComboBox CmbOda;
+        private System.Windows.Forms.ComboBox CmbOdaNo;
         private System.Windows.Forms.MaskedTextBox MskTelefonOgr;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label8;
@@ -384,15 +383,15 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Label label13;
         private System.Windows.Forms.MaskedTextBox MskVeliTelefon;
         private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox TxtVeliAd;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.TextBox TxtVeliSoyad;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- Yurt Otomasyonu/Yurt_Kayit/Yurt_Kayit/Form1.cs
+++ Yurt Otomasyonu/Yurt_Kayit/Yurt_Kayit/Form1.cs
@@ -29,6 +29,41 @@ namespace Yurt_Kayit
                 CmbBolum.Items.Add(oku[0].ToString());
             }
             baglanti.Close();
+            baglanti.Open();
+            SqlCommand komut2 = new SqlCommand("Select Odano From Odalar where OdaKapasite != OdaAktif", baglanti);
+            SqlDataReader oku2 = komut2.ExecuteReader();
+            while(oku2.Read())
+            {
+                CmbOdaNo.Items.Add(oku2[0].ToString());
+            }
+            baglanti.Close();
+
+
+        }
+
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            
+
+                baglanti.Open();
+                SqlCommand komutkaydet = new SqlCommand("insert into Ogrenci (OgrAd,OgrSoyad,OgrTc,OgrTelefon,OgrDogum,OgrBölüm,OgrMail,OgrOdaNo,OgrVeliAd,OgrVeliSoyad,OgrVeliTelefon,OgrVeliAdres) values(@o1,@o2,@o3,@o4,@o5,@o6,@o7,@o8,@v9,@v10,@v11,@v12)", baglanti);
+
+                komutkaydet.Parameters.AddWithValue("@o1", TxtOgrAd.Text);
+                komutkaydet.Parameters.AddWithValue("@o2", TxtOgrSoyad.Text);
+                komutkaydet.Parameters.AddWithValue("@o3", MskTC.Text);
+                komutkaydet.Parameters.AddWithValue("@o4", MskTelefonOgr.Text);
+                komutkaydet.Parameters.AddWithValue("@o5", MskDogumTarihi.Text);
+                komutkaydet.Parameters.AddWithValue("@o6", CmbBolum.Text);
+                komutkaydet.Parameters.AddWithValue("@o7", TxtOgrMail.Text);
+                komutkaydet.Parameters.AddWithValue("@o8", CmbOdaNo.Text);
+                komutkaydet.Parameters.AddWithValue("@v9", TxtVeliAd.Text);
+                komutkaydet.Parameters.AddWithValue("@v10", TxtVeliSoyad.Text);
+                komutkaydet.Parameters.AddWithValue("@v11", MskVeliTelefon.Text);
+                komutkaydet.Parameters.AddWithValue("@v12", richTextBox1.Text);
+
+                komutkaydet.ExecuteNonQuery();
+                baglanti.Close();
+               
 
         }
     }
