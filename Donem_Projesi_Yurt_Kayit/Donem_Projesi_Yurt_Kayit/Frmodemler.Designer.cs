b1500a010ae2cc61819f935eabadb11caba552dd
Ayşegül <115697392+aysegulozden@users.noreply.github.com>
2023-05-06 18:54:07 +0300

    Add files via upload

---
+++ Donem_Projesi_Yurt_Kayit/Donem_Projesi_Yurt_Kayit/Frmodemler.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Donem_Projesi_Yurt_Kayit
+{
+    partial class Frmodemler
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtOgrID = new System.Windows.Forms.TextBox();
+            this.TxtOdenen = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtKalan = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BtnÖdemeAl = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.yurt_KayitDataSet4 = new Donem_Projesi_Yurt_Kayit.Yurt_KayitDataSet4();
+            this.borclarBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.borclarTableAdapter = new Donem_Projesi_Yurt_Kayit.Yurt_KayitDataSet4TableAdapters.BorclarTableAdapter();
+            this.ogrIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ogrAdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ogrSoyadDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ogrKalanBorcDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TxtOgrSoyad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtOgrAd = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yurt_KayitDataSet4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.borclarBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Öğrenci ID:";
+            // 
+            // TxtOgrID
+            // 
+            this.TxtOgrID.Location = new System.Drawing.Point(126, 6);
+            this.TxtOgrID.Name = "TxtOgrID";
+            this.TxtOgrID.Size = new System.Drawing.Size(196, 22);
+            this.TxtOgrID.TabIndex = 1;
+            // 
+            // TxtOdenen
+            // 
+            this.TxtOdenen.Location = new System.Drawing.Point(126, 89);
+            this.TxtOdenen.Name = "TxtOdenen";
+            this.TxtOdenen.Size = new System.Drawing.Size(196, 22);
+            this.TxtOdenen.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ödenen:";
+            // 
+            // TxtKalan
+            // 
+            this.TxtKalan.Location = new System.Drawing.Point(126, 117);
+            this.TxtKalan.Name = "TxtKalan";
+            this.TxtKalan.Size = new System.Drawing.Size(196, 22);
+            this.TxtKalan.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Kalan Borç:";
+            // 
+            // BtnÖdemeAl
+            // 
+            this.BtnÖdemeAl.Location = new System.Drawing.Point(140, 151);
+            this.BtnÖdemeAl.Name = "BtnÖdemeAl";
+            this.BtnÖdemeAl.Size = new System.Drawing.Size(147, 34);
+            this.BtnÖdemeAl.TabIndex = 6;
+            this.BtnÖdemeAl.Text = "Ödeme Al";
+            this.BtnÖdemeAl.UseVisualStyleBackColor = true;
+            this.BtnÖdemeAl.Click += new System.EventHandler(this.BtnÖdemeAl_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ogrIDDataGridViewTextBoxColumn,
+            this.ogrAdDataGridViewTextBoxColumn,
+            this.ogrSoyadDataGridViewTextBoxColumn,
+            this.ogrKalanBorcDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.borclarBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 216);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 204);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // yurt_KayitDataSet4
+            // 
+            this.yurt_KayitDataSet4.DataSetName = "Yurt_KayitDataSet4";
+            this.yurt_KayitDataSet4.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // borclarBindingSource
+            // 
+            this.borclarBindingSource.DataMember = "Borclar";
+            this.borclarBindingSource.DataSource = this.yurt_KayitDataSet4;
+            // 
+            // borclarTableAdapter
+            // 
+            this.borclarTableAdapter.ClearBeforeFill = true;
+            // 
+            // ogrIDDataGridViewTextBoxColumn
+            // 
+            this.ogrIDDataGridViewTextBoxColumn.DataPropertyName = "OgrID";
+            this.ogrIDDataGridViewTextBoxColumn.HeaderText = "OgrID";
+            this.ogrIDDataGridViewTextBoxColumn.Name = "ogrIDDataGridViewTextBoxColumn";
+            // 
+            // ogrAdDataGridViewTextBoxColumn
+            // 
+            this.ogrAdDataGridViewTextBoxColumn.DataPropertyName = "OgrAd";
+            this.ogrAdDataGridViewTextBoxColumn.HeaderText = "OgrAd";
+            this.ogrAdDataGridViewTextBoxColumn.Name = "ogrAdDataGridViewTextBoxColumn";
+            // 
+            // ogrSoyadDataGridViewTextBoxColumn
+            // 
+            this.ogrSoyadDataGridViewTextBoxColumn.DataPropertyName = "OgrSoyad";
+            this.ogrSoyadDataGridViewTextBoxColumn.HeaderText = "OgrSoyad";
+            this.ogrSoyadDataGridViewTextBoxColumn.Name = "ogrSoyadDataGridViewTextBoxColumn";
+            // 
+            // ogrKalanBorcDataGridViewTextBoxColumn
+            // 
+            this.ogrKalanBorcDataGridViewTextBoxColumn.DataPropertyName = "OgrKalanBorc";
+            this.ogrKalanBorcDataGridViewTextBoxColumn.HeaderText = "OgrKalanBorc";
+            this.ogrKalanBorcDataGridViewTextBoxColumn.Name = "ogrKalanBorcDataGridViewTextBoxColumn";
+            // 
+            // TxtOgrSoyad
+            // 
+            this.TxtOgrSoyad.Location = new System.Drawing.Point(126, 62);
+            this.TxtOgrSoyad.Name = "TxtOgrSoyad";
+            this.TxtOgrSoyad.Size = new System.Drawing.Size(196, 22);
+            this.TxtOgrSoyad.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Soyadı:";
+            // 
+            // TxtOgrAd
+            // 
+            this.TxtOgrAd.Location = new System.Drawing.Point(126, 34);
+            this.TxtOgrAd.Name = "TxtOgrAd";
+            this.TxtOgrAd.Size = new System.Drawing.Size(196, 22);
+            this.TxtOgrAd.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(87, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Adı:";
+            // 
+            // Frmodemler
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(445, 420);
+            this.Controls.Add(this.TxtOgrSoyad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtOgrAd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.BtnÖdemeAl);
+            this.Controls.Add(this.TxtKalan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtOdenen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtOgrID);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Lucida Sans", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Frmodemler";
+            this.Text = "Ödemler";
+            this.Load += new System.EventHandler(this.Frmodemler_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yurt_KayitDataSet4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.borclarBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtOgrID;
+        private System.Windows.Forms.TextBox TxtOdenen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtKalan;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BtnÖdemeAl;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Yurt_KayitDataSet4 yurt_KayitDataSet4;
+        private System.Windows.Forms.BindingSource borclarBindingSource;
+        private Yurt_KayitDataSet4TableAdapters.BorclarTableAdapter borclarTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ogrIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ogrAdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ogrSoyadDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ogrKalanBorcDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox TxtOgrSoyad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtOgrAd;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Donem_Projesi_Yurt_Kayit/Donem_Projesi_Yurt_Kayit/Frmodemler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Donem_Projesi_Yurt_Kayit
+{
+    public partial class Frmodemler : Form
+    {
+        public Frmodemler()
+        {
+            InitializeComponent();
+        }
+        SqlBaglantim bgl = new SqlBaglantim();
+
+        private void Frmodemler_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'yurt_KayitDataSet4.Borclar' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.borclarTableAdapter.Fill(this.yurt_KayitDataSet4.Borclar);
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen;
+            string id,  ad, soyad, kalan;
+            secilen = dataGridView1.SelectedCells[0].RowIndex;
+            id = dataGridView1.Rows[secilen].Cells[0].Value.ToString();
+            ad = dataGridView1.Rows[secilen].Cells[1].Value.ToString();
+            soyad = dataGridView1.Rows[secilen].Cells[2].Value.ToString();
+            kalan = dataGridView1.Rows[secilen].Cells[3].Value.ToString();
+            TxtOgrAd.Text = ad;
+            TxtOgrSoyad.Text = soyad;
+            TxtKalan.Text = kalan;
+            TxtOgrID.Text = id;
+
+        }
+
+        private void BtnÖdemeAl_Click(object sender, EventArgs e)
+        {
+            int odenen, kalan,yeniborc;
+            odenen = Convert.ToInt16(TxtOdenen.Text);
+            kalan = Convert.ToInt16(TxtKalan.Text);
+            yeniborc = kalan - odenen;
+
+            TxtKalan.Text = yeniborc.ToString();
+
+            SqlCommand komut = new SqlCommand("update Borclar set Ogrkalanborc=@o1 where OgrID=@o2", bgl.baglanti());
+            komut.Parameters.AddWithValue("@o1", TxtKalan.Text);
+            komut.Parameters.AddWithValue("@o2", TxtOgrID.Text);
+            komut.ExecuteNonQuery();
+            bgl.baglanti().Close();
+            MessageBox.Show("Ödeme Alınmıştır");
+            this.borclarTableAdapter.Fill(this.yurt_KayitDataSet4.Borclar);
+
+
+        }
+    }
+}
